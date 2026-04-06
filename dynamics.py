d4cbc2ee05ffc8fc7ecb7661a923faf5f8710a5f
Adil Faisal <adilfaisal01@proton.me>
2026-04-06 14:54:31 -0400

    added R_bar

--- dynamics.py
+++ dynamics.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numpy._core.shape_base import block
 import osqp
+from scipy.sparse import block_diag
 
 n=2 #number of assets
 H=3 #how many days am I looking ahead
@@ -51,12 +53,20 @@ for j in range(H):
 # print(S_bar.shape)
 # print(T_bar.shape)
 
-Q_bar_top=np.zeros((1,n+1))
-Q_bar_bottom=np.hstack((np.zeros((n,1)),r_hat_cov))
-Q_bar=np.vstack((Q_bar_top,Q_bar_bottom))
+Q_top=np.zeros((1,n+1))
+Q_bottom=np.hstack((np.zeros((n,1)),r_hat_cov))
+Q=np.vstack((Q_top,Q_bottom))
 
-print(Q_bar.shape)    
-assert Q_bar.shape==(n+1,n+1) #making the cost matrices from the covariances of the returns
+print(Q.shape)    
+assert Q.shape==(n+1,n+1) #making the cost matrices from the covariances of the returns
+print(f'Q={Q}')
+# stacking the Q matrices over time horizon, H, since only the diagonals have the Q matrices and the rest are zero, the Q_bar is a sparse block diagonal
+Q_bar=block_diag([Q for _ in range(H)],format='csc')
+print(f'Q_bar:{Q_bar}') # Q_bar is the block sparse matrix for the full cost vector
 
+## Transaction costs, R, for simplicity assume identity matrix
+R=np.eye(n)
+R_bar=block_diag([R for _ in range(H)],format='csc')
+print(f'R_bar:{R_bar}')
 
 
