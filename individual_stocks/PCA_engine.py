9c446a1ab9f8246e793a55c9bf3d6ce9dc32a5c8
Adil Faisal <adilfaisal01@proton.me>
2026-05-21 19:30:03 -0400

    feat: add PCA preprocessing for stock data analysis

--- individual_stocks/PCA_engine.py
+++ individual_stocks/PCA_engine.py
@@ -3,4 +3,11 @@ from sklearn.decomposition import PCA
 
 # reading the dataset
 dataset=pd.read_parquet('final_dataset_stocks.parquet')
+returns_pivot=dataset.pivot_table(values='log_return',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
+spread_pivot=dataset.pivot_table(values='hl_spread',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
+volume_pivot=dataset.pivot_table(values='RVOL',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
 
+## doing PCA calculations
+
+pca=PCA(n_components=12,svd_solver='full')
+pca.fit()
