0ac001ef6ee748ba83602e79f4739715b439d3f0
Adil Faisal <adilfaisal01@proton.me>
2026-05-21 20:30:03 -0400

    feat: enhance PCA analysis with visualization and numpy support

--- individual_stocks/PCA_engine.py
+++ individual_stocks/PCA_engine.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
 
 # reading the dataset
 dataset=pd.read_parquet('final_dataset_stocks.parquet')
+
+##
+
 returns_pivot=dataset.pivot_table(values='log_return',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
 spread_pivot=dataset.pivot_table(values='hl_spread',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
 volume_pivot=dataset.pivot_table(values='RVOL',index='date',columns='ticker').dropna(axis=1,thresh=2000).fillna(0)
-
-## doing PCA calculations
-
-pca_returns=PCA(n_components=12,svd_solver='full')
-print(pca_returns.fit(returns_pivot).explained_variance_ratio_)
