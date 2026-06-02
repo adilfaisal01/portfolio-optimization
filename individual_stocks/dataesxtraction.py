877a82d33c85bb859fddbb3cf69b4cdf09616ff6
Adil Faisal <adilfaisal01@proton.me>
2026-06-02 17:00:03 -0400

    fix: correct typo in pivot_table method and update debug print statement

--- individual_stocks/dataesxtraction.py
+++ individual_stocks/dataesxtraction.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 xx=pd.read_parquet('sector_etf_clean_testingset.parquet')
-print([xx[xx['ticker']=='GLD']['log_return'],xx[xx['ticker']=='TIP']['log_return']])
+print(xx.columns)
 
 class DataExtractor:
     def __init__(self, ticker,macro_indices,dataset='sector_etf_clean_trainingset.parquet'):
@@ -16,7 +16,7 @@ class DataExtractor:
     def get_macro(self):
         macro_data=self.dataset_pframe[self.dataset_pframe['ticker'].isin(self.macro_indices)]
         macro_logreturn=macro_data.pivot_table(index='date', columns='ticker',values='log_return')
-        macro_spread=macro_data.p[ivot_table]
+        macro_spread=macro_data.pivot_table(index='date', columns='ticker', values='hl_spread')
         
         
 
