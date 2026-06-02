bba7219cc37f8c137053c8c07079054ed4821936
Adil Faisal <adilfaisal01@proton.me>
2026-06-02 19:00:02 -0400

    refactor: fix typo in filename and improve DataExtractor class with VIX support

--- individual_stocks/dataextraction.py
+++ individual_stocks/dataextraction.py
@@ -1,19 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-xx=pd.read_parquet('sector_etf_clean_testingset.parquet')
-print(xx.columns)
-
 class DataExtractor:
     def __init__(self, ticker_list,macro_indices,dataset:str='sector_etf_clean_trainingset.parquet'):
-        self.dataset_pframe=pd.read_parquet(dataset)
+        self.dataset_pframe:pd.DataFrame = pd.read_parquet(dataset)
         if macro_indices is None:
             self.macro_indices=['GLD','USO','IYR','SHV','TIP']
         else:
             self.macro_indices=macro_indices
 
         if ticker_list is None:
-            self.ticker_list=['XLK','XLF','XLE','XLV','XLI','XLP','XLY','XLU','XLB','VNQ','VOQ']
+            self.ticker_list=['XLK','XLF','XLE','XLV','XLI','XLP','XLY','XLU','XLB','VNQ','VOX']
         else:
             self.ticker_list=ticker_list
 
@@ -26,8 +23,14 @@ class DataExtractor:
         return macro_logreturn, macro_spread, macro_rvol
 
     def get_assets(self):
-        etf_data=self.dataset_pframe[self.dataset_pframe_pframe['ticker'].isin(self.ticker_list)]
-        etf_longrun=etf_data.pivot_table()
-        
-        
+        etf_data=self.dataset_pframe[self.dataset_pframe['ticker'].isin(self.ticker_list)]
+        etf_logreturn=etf_data.pivot_table(index='date', columns='ticker',values='log_return')
+        etf_spread=etf_data.pivot_table(index='date', columns='ticker', values='hl_spread')
+        etf_rvol=etf_data.pivot_table(index='date',columns='ticker',values='RVOL')
+        return etf_logreturn, etf_spread, etf_rvol
+
+    def get_vix(self,vix_fairweather):
+        vix_data=self.dataset_pframe[self.dataset_pframe['ticker']=='^VIX']['Close']
+        vix_normalized= vix_data/vix_fairweather
+        return vix_normalized
 
--- individual_stocks/stock_env.py
+++ individual_stocks/stock_env.py
@@ -2,14 +2,15 @@ import gymnasium as gym
 import numpy as np
 import pandas as pd
 from gymnasium import spaces
+from dataextraction import DataExtractor
 
 class Portfoliomarket(gym.Env):
     def __init__(self,n_assets:int, n_macro_indicators:int ,max_alloc:float, start_cap:float) -> None:
         self.n_assets=n_assets
-        self.macro_indices=n_macro_indicators
+        self.n_macro_indices=n_macro_indicators
         self.max_w=max_alloc
         self.start_cap=start_cap
-        
+        self.data=DataExtractor(ticker_list=None,macro_indices=None)
     def step(self,u):
         pass
 
