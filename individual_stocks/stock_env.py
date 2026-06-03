dc52cbe42238fa056622251efa31ead8e2c983ea
Adil Faisal <adilfaisal01@proton.me>
2026-06-02 21:30:03 -0400

    feat: enhance portfolio environment with macro and etf volatility tracking

--- individual_stocks/stock_env.py
+++ individual_stocks/stock_env.py
@@ -12,7 +12,9 @@ class Portfoliomarket(gym.Env):
         self.start_cap=start_cap
         self.data=DataExtractor(ticker_list=None, macro_indices=None)
     def step(self,u):
-        macro_lr, macro_sp,ma
+        self.macro_lr, self.macro_sp,self.macro_rvol= self.data.get_macro()
+        self.etf_lr, self.etf_sp, self.etf_rvol= self.data.get_assets()
+        vix_data
 
     def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
         return super().reset(seed=seed, options=options)
